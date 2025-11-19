214a736d88a5183dbff4421f47791fa772d37152
deva <119063711+ndevasaikumar@users.noreply.github.com>
2025-11-19 13:47:33 +0530

    Keep Multiplying Found Values by Two

---
+++ leetcode_2154.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        int i  = 0; 
+        while(i < nums.size()){
+            if(nums[i] == original ){
+                i = 0;
+                original = original * 2;
+            }
+            else{
+                i++;
+            }
+        }
+        return original;
+    }
+};
